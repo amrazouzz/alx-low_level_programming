33c14aebfa9b96b1e15ffb815beb42cc84ee13ec
amrazouzz <amrsalahazouz@gmail.com>
2022-12-09 16:52:09 -0800

    #

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ *Return: Always zero
+ */
+
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar("\n");
+	return (0);
+}
