6c0cc793fe218f93ba94a0b413cfd8cfcf0093d4
amrazouzz <amrsalahazouz@gmail.com>
2022-12-09 17:31:34 -0800

    #

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ *Return: Always zero
+ */
+
+int main(void)
+{
+	int i,e;
+
+	i=48;
+	e=48;
+
+	while (e < 58)
+	{
+		i=48;
+		while (i <58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+
+	}
+
+	putchar('\n');
+	return (0);
+}
