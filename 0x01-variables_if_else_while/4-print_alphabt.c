6168b17175bf356b40b9911864fa7f2bd603995e
amrazouzz <amrsalahazouz@gmail.com>
2022-12-09 17:00:37 -0800

    #

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stlib.h>
+#include<string.h>
+#include<stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ *Return: Always zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(a[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
