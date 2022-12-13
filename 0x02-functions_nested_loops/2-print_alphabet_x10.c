42d89fbaf6690aba940939dd1f7f6d4aed662cd9
amrazouzz <amrsalahazouz@gmail.com>
2022-12-13 15:38:58 -0800

    #

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  * Return: Always 0 (success)
  */
 
 void print_alphabet_x10(void)
 {
 	int str, i;
-	i = 0;
 
+	i = 0;
 	while (i < 10)
 	{
 		for (str = 97; str <= 122; str++)
