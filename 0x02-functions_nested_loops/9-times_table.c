99de3b3c4f293f12f250e7e063160a23dc8058cd
amrazouzz <amrsalahazouz@gmail.com>
2022-12-13 16:53:05 -0800

    #

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Return: no return
+ */	
+void times_table(void)
+{
+	int a, b, op;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+	}
+}
