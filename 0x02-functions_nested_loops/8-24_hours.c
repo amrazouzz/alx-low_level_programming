4eeadad910160ad622a2e4ec29383f3473a541ef
amrazouzz <amrsalahazouz@gmail.com>
2022-12-13 16:33:37 -0800

    #

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,19 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 48; i <= 50; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 48; j <= 57; j++)
 		{
-			for (k = 0; k <= 5; k++)
+			for (k = 48; k <= 53; k++)
 			{
-				for (l = 0; l <= 9; l++)
+				for (l = 48; l <= 57; l++)
 				{
-					printf("%d%d:%d%d\n", i, j, k, l);
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
 				}
 			}
 		}
