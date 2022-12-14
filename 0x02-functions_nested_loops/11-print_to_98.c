ebf5087d7fa5706620a7e3fa1642060ac04cd23f
amrazouzz <amrsalahazouz@gmail.com>
2022-12-13 17:13:35 -0800

    #

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,22 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 0)
+			{
+				printf(", ")
+			}
 		}
 	}
 }
