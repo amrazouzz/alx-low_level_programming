b7eae73540b3f1f2f957c24d83f37896673a2330
amrazouzz <amrsalahazouz@gmail.com>
2022-12-15 10:13:59 -0800

    #

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers.
+ *
+ * return: always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
