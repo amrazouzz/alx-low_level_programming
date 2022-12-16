ff243fcb6cbda6f2e20280547f7c88323986aea5
amrazouzz <amrsalahazouz@gmail.com>
2022-12-16 03:43:26 -0800

    #

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,10 @@ int main(void)
 
 	printf("Infinte loop incoming :(\n");
 	i = 0;
-
+	/*while (i < 10)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 	printf("Infinte loop avoided! \\o/\n");
 	return (0);
 }
