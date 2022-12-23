d90b8aed96d2b4a0a7e02304a8d0d47967a4e047
amrazouzz <amrsalahazouz@gmail.com>
2022-12-22 18:22:46 -0800

    #

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,12 @@
  * _strcmp - compares two strings
  * @s1: first string.
  * @s2: second string.
- * Return: the pointer to dest.
+ * Return: 0 if s1 and s2 are equals,
  * another number if not.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i =0, op = 0;
+	int i = 0, op = 0;
 
 	while (op == 0)
 	{
